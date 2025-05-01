b25c7970a0227b7d2b4c0e91327ab12ff64fd47c
Zohaib8989 <zohaib8989@gmail.com>
2025-05-01 16:08:28 +0500

    Changes made

--- src/userConfig.js
+++ src/userConfig.js
@@ -24,7 +24,7 @@ const userDashboards = {
         companyName: "Zircon Accessories"
     },
     "zohaib8989@gmail.com": {
-        dashboardUrl: "https://app.powerbi.com/view?r=eyJrIjoiMDcxZTU0YjctNmI2YS00MWY2LTg2MmQtZDQ5ZmExNGQ2Y2ViIiwidCI6ImM3NzU3Njk5LWZmZTMtNDhlNi04ZWY2LTUyY2I1ZWQyNDgxMSIsImMiOjl9",
+        dashboardUrl: "https://app.powerbi.com/view?r=eyJrIjoiZGNhYjVmZDYtZGNiYy00NjdhLTgyNTUtODdjYTE5MjQ4OGIyIiwidCI6ImM3NzU3Njk5LWZmZTMtNDhlNi04ZWY2LTUyY2I1ZWQyNDgxMSIsImMiOjl9",
         companyName: "Wealth Track"
     }
 };
