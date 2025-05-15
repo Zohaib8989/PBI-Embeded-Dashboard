1c77eb9e5a329f479e9ae85a59baa5ebd794102f
Zohaib8989 <zohaib8989@gmail.com>
2025-05-15 13:01:32 +0500

    Added Clear Fliters button to the products bage for Zircon

--- src/userConfig.js
+++ src/userConfig.js
@@ -12,15 +12,15 @@ const userDashboards = {
         companyName: "Limitless Life"
     },
     "khalid@algooru.com": {
-        dashboardUrl: "https://app.powerbi.com/view?r=eyJrIjoiNTk0MjQ2YTctMDg4Yi00ZTAxLThlZjAtZTE4ZDFjYzg4ZTg0IiwidCI6ImM3NzU3Njk5LWZmZTMtNDhlNi04ZWY2LTUyY2I1ZWQyNDgxMSIsImMiOjl9",
+        dashboardUrl: "https://app.powerbi.com/view?r=eyJrIjoiYzIxNmQ5NGEtMWYwNy00ZWI3LWJhZDQtOGFhZWZiN2JiMzcyIiwidCI6ImM3NzU3Njk5LWZmZTMtNDhlNi04ZWY2LTUyY2I1ZWQyNDgxMSIsImMiOjl9",
         companyName: "Zircon Accessories"
     },
     "amr@zirconaccessories.com": {
-        dashboardUrl: "https://app.powerbi.com/view?r=eyJrIjoiNTk0MjQ2YTctMDg4Yi00ZTAxLThlZjAtZTE4ZDFjYzg4ZTg0IiwidCI6ImM3NzU3Njk5LWZmZTMtNDhlNi04ZWY2LTUyY2I1ZWQyNDgxMSIsImMiOjl9",
+        dashboardUrl: "https://app.powerbi.com/view?r=eyJrIjoiYzIxNmQ5NGEtMWYwNy00ZWI3LWJhZDQtOGFhZWZiN2JiMzcyIiwidCI6ImM3NzU3Njk5LWZmZTMtNDhlNi04ZWY2LTUyY2I1ZWQyNDgxMSIsImMiOjl9",
         companyName: "Zircon Accessories"
     },
     "husam@zirconaccessories.com": {
-        dashboardUrl: "https://app.powerbi.com/view?r=eyJrIjoiNTk0MjQ2YTctMDg4Yi00ZTAxLThlZjAtZTE4ZDFjYzg4ZTg0IiwidCI6ImM3NzU3Njk5LWZmZTMtNDhlNi04ZWY2LTUyY2I1ZWQyNDgxMSIsImMiOjl9",
+        dashboardUrl: "https://app.powerbi.com/view?r=eyJrIjoiYzIxNmQ5NGEtMWYwNy00ZWI3LWJhZDQtOGFhZWZiN2JiMzcyIiwidCI6ImM3NzU3Njk5LWZmZTMtNDhlNi04ZWY2LTUyY2I1ZWQyNDgxMSIsImMiOjl9",
         companyName: "Zircon Accessories"
     },
     "JaysinAnderson@elevateaccounting.pt": {
